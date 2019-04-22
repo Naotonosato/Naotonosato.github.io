288f1e1b3846868fb04ef343cf623e8e44e3d218
Naotonosato <33174108+Naotonosato@users.noreply.github.com>
2019-04-22 20:57:42 +0900

    Add files via upload

---
+++ generate.js
@@ -0,0 +1,13 @@
+function generate(keyword)
+{
+    const request = new XMLHttpRequest();
+    request.open("GET",`http://m.liveatc.net/feeds/?icao=${keyword}`);
+    request.addEventListener(
+        "load",(event) =>
+    {
+        document.write(event.target.responseText);
+        console.log(event.target.responseText);
+    })
+    request.send();
+}
+generate("NRT");
